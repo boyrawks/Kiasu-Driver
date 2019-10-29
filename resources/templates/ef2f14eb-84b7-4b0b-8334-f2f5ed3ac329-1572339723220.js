0e074e8c88986df1e234feac9e62c38f316153ac
andrewliew2018 <54571918+andrewliew2018@users.noreply.github.com>
2019-10-29 17:04:45 +0800

    Updated Savings Graph and Far East Map

---
+++ resources/templates/ef2f14eb-84b7-4b0b-8334-f2f5ed3ac329-1572339723220.js
@@ -0,0 +1,37 @@
+jQuery("#simulation")
+  .on("pageload", ".t-ef2f14eb-84b7-4b0b-8334-f2f5ed3ac329 .pageload", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#t-Text_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": [ "#t-Text_2" ],
+                    "value": {
+                      "action": "jimSubstring",
+                      "parameter": [ {
+                        "action": "jimSystemTime"
+                      },"0","5" ]
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
